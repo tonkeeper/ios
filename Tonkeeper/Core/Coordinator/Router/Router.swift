bb74f763f131e76a83d79224cfee012082c3f690
Grigory Serebryanyy <grishamsc@gmail.com>
2023-05-23 12:56:07 +0200

    Routing
    - add Presentable
    - add Router

---
+++ Tonkeeper/Core/Coordinator/Router/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  Tonkeeper
+//
+//  Created by Grigory on 23.5.23..
+//
+
+import UIKit
+
+protocol RouterProtocol<RootViewController> {
+  associatedtype RootViewController: UIViewController
+  var rootViewController: RootViewController { get }
+}
+
+final class Router<RootViewController: UIViewController>: RouterProtocol {
+  let rootViewController: RootViewController
+  
+  init(rootViewController: RootViewController) {
+    self.rootViewController = rootViewController
+  }
+  
+  func present(_ presentable: Presentable,
+               options: RouteOptions = .default,
+               completion: (() -> Void)? = nil) {
+    rootViewController.present(presentable.viewController,
+                               animated: options.isAnimated,
+                               completion: completion)
+  }
+  
+  func dismiss(options: RouteOptions = .default,
+               completion: (() -> Void)? = nil) {
+    rootViewController.dismiss(animated: options.isAnimated,
+                               completion: completion)
+  }
+}
