ea2c6fd741a37f527f19d59076ad24e7d2159143
Grigory Serebryanyy <grishamsc@gmail.com>
2023-05-25 14:20:56 +0200

    UIKit
    - add Button
    - add Button.Configuration for plain buttons

--- Tonkeeper/Library/Extensions/UIKit/UIColor+Tonkeeper.swift
+++ Tonkeeper/Library/Extensions/UIKit/UIColor+Tonkeeper.swift
@@ -9,58 +9,64 @@ import UIKit
 
 extension UIColor {
   enum Background {
-    static let contentAttention = UIColor(named: "Colors/Background/Content Attention")
-    static let contentTint = UIColor(named: "Colors/Background/Content Tint")
-    static let content = UIColor(named: "Colors/Background/Content")
-    static let highlighted = UIColor(named: "Colors/Background/Highlighted")
-    static let overlayExtraLight = UIColor(named: "Colors/Background/Overlay Extra Light")
-    static let overlayLight = UIColor(named: "Colors/Background/Overlay Light")
-    static let overlayStrong = UIColor(named: "Colors/Background/Overlay Strong")
-    static let page = UIColor(named: "Colors/Background/Page")
-    static let transparent = UIColor(named: "Colors/Background/Transparent")
+    static let contentAttention = UIColor.named("Colors/Background/Content Attention")
+    static let contentTint = UIColor.named("Colors/Background/Content Tint")
+    static let content = UIColor.named("Colors/Background/Content")
+    static let highlighted = UIColor.named("Colors/Background/Highlighted")
+    static let overlayExtraLight = UIColor.named("Colors/Background/Overlay Extra Light")
+    static let overlayLight = UIColor.named("Colors/Background/Overlay Light")
+    static let overlayStrong = UIColor.named("Colors/Background/Overlay Strong")
+    static let page = UIColor.named("Colors/Background/Page")
+    static let transparent = UIColor.named("Colors/Background/Transparent")
   }
   enum Button {
-    static let primaryBackgroundDisabled = UIColor(named: "Colors/Button/Primary Background Disabled")
-    static let primaryBackgroundHighlighted = UIColor(named: "Colors/Button/Primary Background Highlighted")
-    static let primaryBackground = UIColor(named: "Colors/Button/Primary Background")
-    static let primaryForeground = UIColor(named: "Colors/Button/Primary Foreground")
-    static let secondaryBackgroundDisabled = UIColor(named: "Colors/Button/Secondary Background Disabled")
-    static let secondaryBackgroundHighlighted = UIColor(named: "Colors/Button/Secondary Background Highlighted")
-    static let secondaryBackground = UIColor(named: "Colors/Button/Secondary Background")
-    static let secondaryForeground = UIColor(named: "Colors/Button/Secondary Foreground")
-    static let tertiaryBackgroundDisabled = UIColor(named: "Colors/Button/Tertiary Background Disabled")
-    static let tertiaryBackgroundHighlighted = UIColor(named: "Colors/Button/Tertiary Background Highlighted")
-    static let tertiaryBackground = UIColor(named: "Colors/Button/Tertiary Background")
-    static let tertiaryForeground = UIColor(named: "Colors/Button/Tertiary Foreground")
+    static let primaryBackgroundDisabled = UIColor.named("Colors/Button/Primary Background Disabled")
+    static let primaryBackgroundHighlighted = UIColor.named("Colors/Button/Primary Background Highlighted")
+    static let primaryBackground = UIColor.named("Colors/Button/Primary Background")
+    static let primaryForeground = UIColor.named("Colors/Button/Primary Foreground")
+    static let secondaryBackgroundDisabled = UIColor.named("Colors/Button/Secondary Background Disabled")
+    static let secondaryBackgroundHighlighted = UIColor.named("Colors/Button/Secondary Background Highlighted")
+    static let secondaryBackground = UIColor.named("Colors/Button/Secondary Background")
+    static let secondaryForeground = UIColor.named("Colors/Button/Secondary Foreground")
+    static let tertiaryBackgroundDisabled = UIColor.named("Colors/Button/Tertiary Background Disabled")
+    static let tertiaryBackgroundHighlighted = UIColor.named("Colors/Button/Tertiary Background Highlighted")
+    static let tertiaryBackground = UIColor.named("Colors/Button/Tertiary Background")
+    static let tertiaryForeground = UIColor.named("Colors/Button/Tertiary Foreground")
   }
   enum Field {
-    static let activeBorder = UIColor(named: "Colors/Field/Active Border")
-    static let background = UIColor(named: "Colors/Field/Background")
-    static let errorBackground = UIColor(named: "Colors/Field/Error Background")
-    static let errorBorder = UIColor(named: "Colors/Field/Error Border")
+    static let activeBorder = UIColor.named("Colors/Field/Active Border")
+    static let background = UIColor.named("Colors/Field/Background")
+    static let errorBackground = UIColor.named("Colors/Field/Error Background")
+    static let errorBorder = UIColor.named("Colors/Field/Error Border")
   }
   enum Icon {
-    static let primaryAlternate = UIColor(named: "Colors/Icon/Primary Alternate")
-    static let primary = UIColor(named: "Colors/Icon/Primary")
-    static let secondary = UIColor(named: "Colors/Icon/Secondary")
-    static let tertiary = UIColor(named: "Colors/Icon/Tertiary")
+    static let primaryAlternate = UIColor.named("Colors/Icon/Primary Alternate")
+    static let primary = UIColor.named("Colors/Icon/Primary")
+    static let secondary = UIColor.named("Colors/Icon/Secondary")
+    static let tertiary = UIColor.named("Colors/Icon/Tertiary")
   }
   enum Separator {
-    static let alternate = UIColor(named: "Colors/Separator/Alternate")
-    static let common = UIColor(named: "Colors/Separator/Common")
+    static let alternate = UIColor.named("Colors/Separator/Alternate")
+    static let common = UIColor.named("Colors/Separator/Common")
   }
   enum TabBar {
-    static let activeIcon = UIColor(named: "Colors/TabBar/Active Icon")
-    static let inactiveIcon = UIColor(named: "Colors/TabBar/Inactive Icon")
+    static let activeIcon = UIColor.named("Colors/TabBar/Active Icon")
+    static let inactiveIcon = UIColor.named("Colors/TabBar/Inactive Icon")
   }
   enum Text {
-    static let accent = UIColor(named: "Colors/Text/Accent")
-    static let primaryAlternate = UIColor(named: "Colors/Text/Primary Alternate")
-    static let primary = UIColor(named: "Colors/Text/Primary")
-    static let secondary = UIColor(named: "Colors/Text/Secondary")
-    static let tertiary = UIColor(named: "Colors/Text/Tertiary")
+    static let accent = UIColor.named("Colors/Text/Accent")
+    static let primaryAlternate = UIColor.named("Colors/Text/Primary Alternate")
+    static let primary = UIColor.named("Colors/Text/Primary")
+    static let secondary = UIColor.named("Colors/Text/Secondary")
+    static let tertiary = UIColor.named("Colors/Text/Tertiary")
   }
   enum Accent {
-    static let blue = UIColor(named: "Colors/Accent/Blue")
+    static let blue = UIColor.named("Colors/Accent/Blue")
+  }
+  
+  private static func named(_ name: String) -> UIColor {
+    let color = UIColor(named: name)
+    assert(color != nil, "Can't load color with name: \(name)")
+    return color ?? .black
   }
 }
---
+++ Tonkeeper/Library/UIKit/Buttons/Button.swift
@@ -0,0 +1,274 @@
+//
+//  Button.swift
+//  Tonkeeper
+//
+//  Created by Grigory on 25.5.23..
+//
+
+import UIKit
+
+extension UIControl.State: Hashable {}
+
+final class Button: UIControl {
+  
+  enum `Type` {
+    case primary
+    case secondary
+    case tertiary
+    
+    var backgroundColors: [UIControl.State: UIColor] {
+      switch self {
+      case .primary:
+        return [.normal: .Button.primaryBackground,
+                .highlighted: .Button.primaryBackgroundHighlighted,
+                .disabled: .Button.primaryBackgroundDisabled]
+      case .secondary:
+        return [.normal: .Button.secondaryBackground,
+                .highlighted: .Button.secondaryBackgroundHighlighted,
+                .disabled: .Button.secondaryBackgroundDisabled]
+      case .tertiary:
+        return [.normal: .Button.tertiaryBackground,
+                .highlighted: .Button.tertiaryBackgroundHighlighted,
+                .disabled: .Button.tertiaryBackgroundDisabled]
+      }
+    }
+    
+    var tintColors: [UIControl.State: UIColor] {
+      let colors: [UIControl.State: UIColor] = [
+        .normal: .Button.primaryForeground,
+        .highlighted: .Button.primaryForeground,
+        .disabled: .Button.primaryForeground.withAlphaComponent(0.48)
+      ]
+      return colors
+    }
+  }
+  
+  enum Size {
+    case xsmall
+    case small
+    case welter
+    case medium
+    case large
+    case custom(height: CGFloat, cornerRadius: CGFloat, textStyle: TextStyle)
+    
+    var height: CGFloat {
+      switch self {
+      case .xsmall: return 32
+      case .small: return 36
+      case .welter: return 44
+      case .medium: return 48
+      case .large: return 56
+      case let .custom(height, _, _): return height
+      }
+    }
+    
+    var cornerRadius: CGFloat {
+      switch self {
+      case .xsmall: return 0
+      case .small: return 18
+      case .large: return 16
+      case .medium: return 24
+      case .welter: return 0
+      case let .custom(_, cornerRadius, _): return cornerRadius
+      }
+    }
+    
+    var textStyle: TextStyle {
+      switch self {
+      case .xsmall: return .label2
+      case .small: return .label2
+      case .large: return .label1
+      case .medium: return .label1
+      case .welter: return .label1
+      case let .custom(_, _, textStyle): return textStyle
+      }
+    }
+  }
+  
+  enum Shape {
+    case circle
+    case rect
+  }
+  
+  enum IconPosition {
+    case left
+    case right
+  }
+  
+  struct Configuration {
+    let type: `Type`
+    let size: Size
+    let shape: Shape
+    let contentInsets: UIEdgeInsets
+  }
+  
+  var iconPosition: IconPosition = .left {
+    didSet {
+      guard iconPosition != oldValue else { return }
+      updateIconPosition()
+    }
+  }
+ 
+  var configuration: Configuration {
+    didSet {
+      updateConfiguration()
+      invalidateIntrinsicContentSize()
+    }
+  }
+  
+  let titleLabel = UILabel()
+  let iconImageView = UIImageView()
+  let contentStackView = UIStackView()
+
+  private let maskLayer = CAShapeLayer()
+  private var contentLeftConstraint: NSLayoutConstraint?
+  private var contentRightConstraint: NSLayoutConstraint?
+  private var contentTopConstraint: NSLayoutConstraint?
+  private var contentBottomConstraint: NSLayoutConstraint?
+  
+  override var isHighlighted: Bool {
+    didSet {
+      guard isHighlighted != oldValue else { return }
+      updateBackground()
+      updateTintColor()
+    }
+  }
+  
+  override var isEnabled: Bool {
+    didSet {
+      guard isEnabled != oldValue else { return }
+      updateBackground()
+      updateTintColor()
+    }
+  }
+  
+  init(configuration: Configuration) {
+    self.configuration = configuration
+    super.init(frame: .zero)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    maskLayer.frame = bounds
+    let path: UIBezierPath
+    switch configuration.shape {
+    case .circle:
+      path = .init(ovalIn: bounds)
+    case .rect:
+      path = .init(roundedRect: bounds, cornerRadius: configuration.size.cornerRadius)
+    }
+    maskLayer.path = path.cgPath
+    layer.mask = maskLayer
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    let height = configuration.size.height
+    let width: CGFloat
+    switch configuration.shape {
+    case .rect:
+      width = round(contentStackView.systemLayoutSizeFitting(.zero).width)
+    case .circle:
+      width = height
+    }
+    
+    let contentSize = CGSize(
+      width: width + configuration.contentInsets.left + configuration.contentInsets.right,
+      height: height
+    )
+    return contentSize
+  }
+  
+  override func sizeThatFits(_ size: CGSize) -> CGSize {
+    intrinsicContentSize
+  }
+  
+  override func didMoveToWindow() {
+    super.didMoveToWindow()
+    maskLayer.rasterizationScale = 2 * (window?.windowScene?.screen.scale ?? 1)
+  }
+}
+
+private extension Button {
+  func setup() {
+    maskLayer.shouldRasterize = true
+    
+    contentStackView.axis = .horizontal
+    contentStackView.alignment = .center
+    contentStackView.isUserInteractionEnabled = false
+    
+    contentStackView.addArrangedSubview(iconImageView)
+    contentStackView.addArrangedSubview(titleLabel)
+    addSubview(contentStackView)
+    
+    setupConstraints()
+    
+    updateConfiguration()
+    updateBackground()
+    updateIconPosition()
+  }
+  
+  func setupConstraints() {
+    contentStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    let contentInsets = configuration.contentInsets
+    let leftConstraint = contentStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: contentInsets.left)
+    let rightConstraint = contentStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -contentInsets.right)
+    let topConstraint = contentStackView.topAnchor.constraint(equalTo: topAnchor, constant: contentInsets.top)
+    let bottomConstraint = contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -contentInsets.bottom)
+    
+    self.contentTopConstraint = topConstraint
+    self.contentBottomConstraint = bottomConstraint
+    self.contentRightConstraint = rightConstraint
+    self.contentLeftConstraint = leftConstraint
+    
+    leftConstraint.priority = .defaultLow
+    rightConstraint.priority = .defaultLow
+    
+    NSLayoutConstraint.activate([
+      contentStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      topConstraint,
+      bottomConstraint,
+      leftConstraint,
+      rightConstraint
+    ])
+  }
+  
+  func updateBackground() {
+    backgroundColor = configuration.type.backgroundColors[state]
+  }
+  
+  func updateTintColor() {
+    titleLabel.textColor = configuration.type.tintColors[state]
+    iconImageView.tintColor = configuration.type.tintColors[state]
+  }
+  
+  func updateIconPosition() {
+    switch iconPosition {
+    case .left:
+      contentStackView.insertArrangedSubview(iconImageView, at: 0)
+    case .right:
+      contentStackView.insertArrangedSubview(iconImageView, at: 1)
+    }
+  }
+  
+  func updateConfiguration() {
+    updateBackground()
+    updateTintColor()
+    updateContentInsets()
+    titleLabel.applyTextStyleFont(configuration.size.textStyle)
+  }
+  
+  func updateContentInsets() {
+    let contentInsets = configuration.contentInsets
+    contentLeftConstraint?.constant = contentInsets.left
+    contentRightConstraint?.constant = -contentInsets.right
+    contentTopConstraint?.constant = contentInsets.top
+    contentBottomConstraint?.constant = -contentInsets.bottom
+  }
+}
