ade37a177cde6dfa2c8f3835080fe21152eb8356
Grigory Serebryanyy <grishamsc@gmail.com>
2023-06-08 16:51:13 +0200

    UIKit
    - ModalCard add basic layout and dragging logic

--- Tonkeeper/Library/UIKit/ModalCard/ModalCardContainerView.swift
+++ Tonkeeper/Library/UIKit/ModalCard/ModalCardContainerView.swift
@@ -7,4 +7,98 @@
 
 import UIKit
 
-final class ModalCardContainerView: UIView {}
+final class ModalCardContainerView: PassthroughView {
+  
+  var maximumContentHeight: CGFloat {
+    bounds.height
+    - safeAreaInsets.top - safeAreaInsets.bottom
+    - headerView.bounds.height
+  }
+  
+  var contentHeight: CGFloat = 0 {
+    didSet {
+      contentContainerHeightConstraint?.constant = contentHeight
+      mainViewHeightConstraint?.constant = contentHeight + 64
+    }
+  }
+  
+  var dragOffset: CGFloat = 0 {
+    didSet {
+      mainViewHeightConstraint?.constant = contentHeight - dragOffset + 64
+      contentContainerHeightConstraint?.constant = contentHeight - dragOffset
+    }
+  }
+  
+  let contentContainer: UIView = {
+    let view = UIView()
+    view.backgroundColor = .brown
+    return view
+  }()
+  
+  let mainView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .red
+    return view
+  }()
+  
+  let headerView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .green
+    return view
+  }()
+  
+  var mainViewHeightConstraint: NSLayoutConstraint?
+  var contentContainerHeightConstraint: NSLayoutConstraint?
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func addContentView(_ view: UIView) {
+    contentContainer.addSubview(view)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      view.topAnchor.constraint(equalTo: contentContainer.topAnchor),
+      view.leftAnchor.constraint(equalTo: contentContainer.leftAnchor),
+      view.rightAnchor.constraint(equalTo: contentContainer.rightAnchor),
+      view.bottomAnchor.constraint(equalTo: contentContainer.bottomAnchor)
+    ])
+  }
+}
+
+private extension ModalCardContainerView {
+  func setup() {
+    addSubview(mainView)
+    mainView.addSubview(headerView)
+    mainView.addSubview(contentContainer)
+    
+    mainView.translatesAutoresizingMaskIntoConstraints = false
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    contentContainer.translatesAutoresizingMaskIntoConstraints = false
+    
+    mainViewHeightConstraint = mainView.heightAnchor.constraint(equalToConstant: 0)
+    mainViewHeightConstraint?.isActive = true
+    contentContainerHeightConstraint = contentContainer.heightAnchor.constraint(equalToConstant: 0)
+    contentContainerHeightConstraint?.isActive = true
+    
+    NSLayoutConstraint.activate([
+      mainView.bottomAnchor.constraint(equalTo: bottomAnchor),
+      mainView.leftAnchor.constraint(equalTo: leftAnchor),
+      mainView.rightAnchor.constraint(equalTo: rightAnchor),
+      
+      headerView.topAnchor.constraint(equalTo: mainView.topAnchor),
+      headerView.leftAnchor.constraint(equalTo: mainView.leftAnchor),
+      headerView.rightAnchor.constraint(equalTo: mainView.rightAnchor),
+      headerView.heightAnchor.constraint(equalToConstant: 64),
+      
+      contentContainer.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+      contentContainer.leftAnchor.constraint(equalTo: mainView.leftAnchor),
+      contentContainer.rightAnchor.constraint(equalTo: mainView.rightAnchor)
+    ])
+  }
+}
--- Tonkeeper/Library/UIKit/ModalCard/ModalCardContainerViewController.swift
+++ Tonkeeper/Library/UIKit/ModalCard/ModalCardContainerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ModalCardContainerViewController: UIViewController {
+final class ModalCardContainerViewController: GenericViewController<ModalCardContainerView> {
   override var transitioningDelegate: UIViewControllerTransitioningDelegate? {
     get { dimmingTransitioningDelegate }
     set {}
@@ -19,8 +19,125 @@ final class ModalCardContainerViewController: UIViewController {
   }
   
   private let dimmingTransitioningDelegate = DimmingTransitioningDelegate()
+  private let content: ModalCardContainerContent
+  private let panGestureRecognizer = UIPanGestureRecognizer()
   
-  override func loadView() {
-    view = PassthroughView()
+  // MARK: - State
+  
+  private var cachedHeight: CGFloat = 0
+  
+  // MARK: - Init
+  
+  init(content: ModalCardContainerContent) {
+    self.content = content
+    super.init(nibName: nil, bundle: nil)
   }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - View Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    guard cachedHeight != view.bounds.height else { return }
+    cachedHeight = view.bounds.height
+    customView.layoutIfNeeded()
+    updateContentHeight()
+  }
+}
+
+private extension ModalCardContainerViewController {
+  func setup() {
+    setupContent()
+    setupGestures()
+  }
+  
+  func setupContent() {
+    addChild(content)
+    customView.addContentView(content.view)
+    content.didMove(toParent: self)
+    
+    content.didUpdateHeight = { [weak self] in
+      self?.panGestureRecognizer.isEnabled = false
+      self?.updateContentHeight()
+      self?.panGestureRecognizer.isEnabled = true
+    }
+  }
+  
+  func updateContentHeight() {
+    let contentHeight = content.height
+    customView.contentHeight = contentHeight
+    animateLayout()
+  }
+  
+  func setupGestures() {
+    setupPanGesture()
+  }
+  
+  func setupPanGesture() {
+    panGestureRecognizer.addTarget(
+      self,
+      action: #selector(panGestureHandler(_:))
+    )
+    customView.addGestureRecognizer(panGestureRecognizer)
+  }
+  
+  func animateLayout() {
+    UIView.animate(
+        withDuration: 0.4,
+        delay: .zero,
+        usingSpringWithDamping: 2,
+        initialSpringVelocity: 0,
+        options: .curveEaseInOut,
+        animations: customView.layoutIfNeeded
+    )
+  }
+}
+
+private extension ModalCardContainerViewController {
+  @objc
+  func panGestureHandler(_ recognizer: UIPanGestureRecognizer) {
+    let yTranslation = recognizer.translation(in: recognizer.view).y
+    let dragOffset = max(-.maximumDragOffset, yTranslation * .dragOffsetCoefficient)
+    switch recognizer.state {
+    case .changed:
+      didDrag(with: dragOffset)
+    case .ended:
+      let yVelocity = recognizer.velocity(in: recognizer.view).y
+      didEndDragging(offset: dragOffset, velocity: yVelocity)
+    case .cancelled, .failed:
+      didEndDragging(offset: 0, velocity: 0)
+    default:
+      break
+    }
+  }
+  
+  func didDrag(with offset: CGFloat) {
+    customView.dragOffset = offset
+  }
+  
+  func didEndDragging(offset: CGFloat, velocity: CGFloat) {
+    let dragDistance = offset / customView.contentHeight
+    let isDismiss = dragDistance >= .dragTreshold || velocity >= .velocityTreshold
+    if isDismiss {
+      dismiss(animated: true)
+    } else {
+      customView.dragOffset = 0
+      animateLayout()
+    }
+  }
+}
+
+private extension CGFloat {
+  static let maximumDragOffset: CGFloat = 20
+  static let dragOffsetCoefficient: CGFloat = 1/2
+  static let dragTreshold: CGFloat = 1/3
+  static let velocityTreshold: CGFloat = 1500
 }
