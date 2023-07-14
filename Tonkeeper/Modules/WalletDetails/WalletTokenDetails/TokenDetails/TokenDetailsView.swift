70f03a573ee8b76e2591e80bb4a251ede295ff9f
Grigory Serebryanyy <grishamsc@gmail.com>
2023-07-14 22:34:36 +0200

    Token details
    - setup TokenDetailsView

--- Tonkeeper/Modules/WalletDetails/WalletTokenDetails/TokenDetails/TokenDetailsView.swift
+++ Tonkeeper/Modules/WalletDetails/WalletTokenDetails/TokenDetails/TokenDetailsView.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 final class TokenDetailsView: UIView {
+  
+  let scrollView = UIScrollView()
+  let headerView = TokenDetailsHeaderView()
+  let refreshControl = UIRefreshControl()
 
   // MARK: - Init
 
@@ -25,5 +29,32 @@ final class TokenDetailsView: UIView {
 // MARK: - Private
 
 private extension TokenDetailsView {
-  func setup() {}
+  func setup() {
+    refreshControl.tintColor = .Icon.primary
+    
+    scrollView.alwaysBounceVertical = true
+    scrollView.refreshControl = refreshControl
+    
+    addSubview(scrollView)
+    scrollView.addSubview(headerView)
+    setupConstraints()
+  }
+  
+  func setupConstraints() {
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor.constraint(equalTo: topAnchor),
+      scrollView.leftAnchor.constraint(equalTo: leftAnchor),
+      scrollView.rightAnchor.constraint(equalTo: rightAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+      
+      headerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      headerView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+      headerView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+      headerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      headerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+    ])
+  }
 }
