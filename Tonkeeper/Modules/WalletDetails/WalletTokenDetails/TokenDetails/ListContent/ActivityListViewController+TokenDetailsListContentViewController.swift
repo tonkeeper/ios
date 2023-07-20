c3a7281fa65991c59c1fb3dc9f07408379dacc32
Grigory Serebryanyy <grishamsc@gmail.com>
2023-07-20 16:11:36 +0200

    Token details | Activity
    - add TokenDetailsListContentViewController
    - ActivityListViewController conforms to TokenDetailsListContentViewController

---
+++ Tonkeeper/Modules/WalletDetails/WalletTokenDetails/TokenDetails/ListContent/ActivityListViewController+TokenDetailsListContentViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ActivityListViewController+TokenDetailsListContentViewController.swift
+//  Tonkeeper
+//
+//  Created by Grigory on 20.7.23..
+//
+
+import UIKit
+
+extension ActivityListViewController: TokenDetailsListContentViewController { 
+  func setHeaderView(_ headerView: UIView) {
+    customView.setHeaderView(headerView)
+  }
+}
---
+++ Tonkeeper/Modules/WalletDetails/WalletTokenDetails/TokenDetails/ListContent/TokenDetailsListContentViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TokenDetailsListContentViewController.swift
+//  Tonkeeper
+//
+//  Created by Grigory on 20.7.23..
+//
+
+import UIKit
+
+protocol TokenDetailsListContentViewController: UIViewController {
+  var scrollView: UIScrollView { get }
+  func setHeaderView(_ headerView: UIView)
+}
