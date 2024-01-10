26ca08c87bcaf254956c2522a03a20e40ae24ac5
Grigory Serebryanyy <grishamsc@gmail.com>
2024-01-10 17:33:44 +0100

    Firebase
    - add Firebase Analytics
    - add Firebase Crashlytics

--- Tonkeeper/AppDelegate.swift
+++ Tonkeeper/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // TODO: Extract
+    FirebaseApp.configure()
+    
     return true
   }
   
