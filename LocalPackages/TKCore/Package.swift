726b835e488dadb2d8c2cac1ec1054a403b1eb42
Grigory Serebryanyy <grishamsc@gmail.com>
2024-02-07 09:46:08 +0100

    TKCore
    - add ImageDownloadTask

--- LocalPackages/TKCore/Package.swift
+++ LocalPackages/TKCore/Package.swift
@@ -14,13 +14,15 @@ let package = Package(
       targets: ["TKCore"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/onevcat/Kingfisher.git", from: Version(7, 0, 0))
+    .package(url: "https://github.com/onevcat/Kingfisher.git", from: Version(7, 0, 0)),
+    .package(url: "https://github.com/tonkeeper/tkuikit-ios.git", branch: "main")
   ],
   targets: [
     .target(
       name: "TKCore",
       dependencies: [
-        .byName(name: "Kingfisher")
+        .byName(name: "Kingfisher"),
+        .product(name: "TKUIKit", package: "tkuikit-ios")
       ],
       resources: [.process("Resources")]),
     .testTarget(
--- LocalPackages/TKCore/Sources/TKCore/ImageLoader/ImageLoader.swift
+++ LocalPackages/TKCore/Sources/TKCore/ImageLoader/ImageLoader.swift
@@ -1,14 +1,14 @@
 import Kingfisher
 import UIKit
+import TKUIKit
 
 public final class ImageLoader {
   public init() {}
   
-  
   public func loadImage(url: URL?,
                         imageView: UIImageView,
                         size: CGSize? = nil,
-                        cornerRadius: CGFloat? = nil) -> DownloadTask? {
+                        cornerRadius: CGFloat? = nil) -> Kingfisher.DownloadTask? {
     var options = KingfisherOptionsInfo()
     var processor: ImageProcessor = DefaultImageProcessor.default
     
@@ -25,7 +25,26 @@ public final class ImageLoader {
     }
     
     options.append(.processor(processor))
-    
+
     return imageView.kf.setImage(with: url, options: options)
+  }
+}
+
+public class ImageDownloadTask: TKUIKit.ImageDownloadTask {
+  let closure: (UIImageView, CGSize?, CGFloat?) -> Kingfisher.DownloadTask?
+  var downloadTask: Kingfisher.DownloadTask?
+  
+  public init(closure: @escaping (UIImageView, CGSize?, CGFloat?) -> Kingfisher.DownloadTask?) {
+    self.closure = closure
+  }
+  
+  public func start(imageView: UIImageView,
+             size: CGSize? = nil,
+             cornerRadius: CGFloat? = nil) {
+    self.downloadTask = closure(imageView, size, cornerRadius)
+  }
+  
+  public func cancel() {
+    self.downloadTask?.cancel()
   }
 }
