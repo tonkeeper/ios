274c4c16ac20affd4d8c15642a8e07bf08600d34
Grigory Serebryanyy <grishamsc@gmail.com>
2024-05-02 14:32:12 +0200

    Signer import
    - fix plus symbol in public key percent encoding

--- LocalPackages/core-swift/Sources/SignerCore/Deeplink/ExportDeeplinkGenerator.swift
+++ LocalPackages/core-swift/Sources/SignerCore/Deeplink/ExportDeeplinkGenerator.swift
@@ -3,12 +3,19 @@ import TonSwift
 
 struct ExportDeeplinkGenerator {
   func generateDeeplink(network: Network, key: WalletKey) -> URL? {
+    guard let publicKey = key
+      .publicKey.data.base64EncodedString()
+      .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)?
+      .replacingOccurrences(of: "+", with: "%2B") else {
+      return nil
+    }
+    
     var components = URLComponents()
     components.scheme = "tonkeeper"
     components.host = "signer"
     components.path = "/link"
-    components.queryItems = [
-      URLQueryItem(name: "pk", value: key.publicKey.data.base64EncodedString()),
+    components.percentEncodedQueryItems = [
+      URLQueryItem(name: "pk", value: publicKey),
       URLQueryItem(name: "name", value: key.name),
       URLQueryItem(name: "network", value: network.rawValue)
     ]
