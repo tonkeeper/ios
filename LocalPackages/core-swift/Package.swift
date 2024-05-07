1886efa9dbf2ee2189acade0ee5fd3a7e9db6b80
Max Voloshinskii <me@voloshinskii.ru>
2024-05-07 13:34:35 +0300

    Update ton-swift

--- LocalPackages/core-swift/Package.swift
+++ LocalPackages/core-swift/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     .package(path: "../TKLocalize"),
-    .package(url: "https://github.com/tonkeeper/ton-swift", from: "1.0.6"),
+    .package(url: "https://github.com/tonkeeper/ton-swift", from: "1.0.8"),
     .package(url: "https://github.com/tonkeeper/ton-api-swift", from: "0.1.1"),
     .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.3.0"))
   ],
