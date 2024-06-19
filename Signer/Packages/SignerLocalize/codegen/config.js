51aa47416566fff1829f8f5ce3c70337857dd8b0
Grigory Serebryanyy <grishamsc@gmail.com>
2024-06-19 14:47:52 +0200

    Signer outcast
    - remove signer
    - remove workspace

--- Signer/Packages/SignerLocalize/codegen/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  stringsFilePath: '../Sources/SignerLocalize/Resources/Locales/en.lproj/Localizable.strings',
-  codePath: '../Sources/SignerLocalize/SignerLocalize.swift'
-}
-
-module.exports = config;
--- Signer/Signer/Common/Module/Module.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-struct Module<View: UIViewController, Output, Input> {
-  let view: View
-  let output: Output
-  let input: Input
-}
