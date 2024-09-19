c3324feeaa6d0e1cd7b46dd12b14972018fce93c
Grigory Serebryanyy <grishamsc@gmail.com>
2024-09-19 09:23:52 +0200

    WalletCore
    - fix tests

--- LocalPackages/core-swift/Package.swift
+++ LocalPackages/core-swift/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "WalletCore",
   platforms: [
-    .macOS(.v12), .iOS(.v14)
+    .iOS(.v14)
   ],
   products: [
     .library(name: "WalletCore", type: .dynamic, targets: ["KeeperCore"])
