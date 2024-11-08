2031415f1825995de04ff7e16929903c171c9c50
Grigory Serebryanyy <grishamsc@gmail.com>
2024-11-08 18:32:38 +0100

    Dependencies
    - bump ton-swift

--- LocalPackages/core-swift/Package.swift
+++ LocalPackages/core-swift/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .package(path: "../TKCryptoSwift"),
     .package(path: "../TKKeychain"),
     .package(path: "../Ledger"),
-    .package(url: "https://github.com/tonkeeper/ton-swift", .upToNextMinor(from: "1.0.21")),
+    .package(url: "https://github.com/tonkeeper/ton-swift", .upToNextMinor(from: "1.0.22")),
     .package(url: "https://github.com/tonkeeper/URKit", .upToNextMinor(from: "16.0.0")),
     .package(url: "https://github.com/tonkeeper/ton-api-swift", .upToNextMinor(from: "0.3.0")),
     .package(url: "https://github.com/tonkeeper/battery-api-swift", .upToNextMinor(from: "2.0.2")),
