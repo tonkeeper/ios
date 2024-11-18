693d6d346bc7a6c91b298e87fa4758c0c2fc0565
Max Voloshinskii <me@voloshinskii.ru>
2024-11-18 15:47:40 +0300

    Add firebase performance support

--- LocalPackages/App/Sources/App/BrowserModule/Flows/DappCoordinator.swift
+++ LocalPackages/App/Sources/App/BrowserModule/Flows/DappCoordinator.swift
@@ -4,6 +4,7 @@ import TKCore
 import KeeperCore
 import TKScreenKit
 import TKUIKit
+import FirebasePerformance
 
 @MainActor
 final class DappCoordinator: RouterCoordinator<ViewControllerRouter> {
@@ -82,6 +83,7 @@ final class DappCoordinator: RouterCoordinator<ViewControllerRouter> {
     ToastPresenter.hideAll()
     ToastPresenter.showToast(configuration: .loading)
     Task {
+      let trace = Performance.startTrace(name: "perform_connect")
       do {
         let manifest = try await keeperCoreMainAssembly.tonConnectAssembly.tonConnectService().loadManifest(
           url: payload.manifestUrl
@@ -91,6 +93,7 @@ final class DappCoordinator: RouterCoordinator<ViewControllerRouter> {
           clientId: UUID().uuidString,
           requestPayload: payload
         )
+        trace?.setValue(manifest.url.absoluteString, forAttribute: "manifest")
         await MainActor.run {
           ToastPresenter.hideToast()
           handleLoadedManifest(
@@ -100,12 +103,15 @@ final class DappCoordinator: RouterCoordinator<ViewControllerRouter> {
             completion: completion
           )
         }
+        trace?.setValue("success", forAttribute: "result")
       } catch {
         await MainActor.run {
           ToastPresenter.hideToast()
           completion(.error(.appManifestNotFound))
         }
+        trace?.setValue("fail", forAttribute: "result")
       }
+      trace?.stop()
     }
 
     func handleLoadedManifest(parameters: TonConnectParameters,
