326811d905170df15c88964417673742eabeea62
Grigory Serebryanyy <grishamsc@gmail.com>
2024-11-28 22:31:47 +0100

    iOS-247

--- LocalPackages/core-swift/Sources/KeeperCore/Entities/Wallet/Wallet+Extensions.swift
+++ LocalPackages/core-swift/Sources/KeeperCore/Entities/Wallet/Wallet+Extensions.swift
@@ -184,20 +184,7 @@ public extension Wallet {
   }
   
   var isBrowserAvailable: Bool {
-    switch kind {
-    case .regular:
-      return true
-    case .lockup:
-      return false
-    case .watchonly:
-      return false
-    case .signer:
-      return false
-    case .ledger:
-      return false
-    case .keystone:
-      return false
-    }
+    true
   }
   
   var isGaslessAvailable: Bool {
