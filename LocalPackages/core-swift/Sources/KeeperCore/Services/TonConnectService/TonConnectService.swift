996dc5f8a02eb96eff80672a21c6944e03f412a1
Max Voloshinskii <me@voloshinskii.com>
2025-01-30 10:15:13 +0300

    Remove unused code

--- LocalPackages/core-swift/Sources/KeeperCore/Services/TonConnectService/TonConnectService.swift
+++ LocalPackages/core-swift/Sources/KeeperCore/Services/TonConnectService/TonConnectService.swift
@@ -337,15 +337,6 @@ private extension TonConnectServiceImplementation {
       )
     }
     
-    let batteryPayloads = parameters.messagesVariants?.battery.map { message in
-      TransferData.TonConnect.Payload(
-        value: BigInt(integerLiteral: message.amount),
-        recipientAddress: message.address,
-        stateInit: message.stateInit,
-        payload: message.payload
-      )
-    }
-    
     let transferData = TransferData(
       transfer: .tonConnect(
         TransferData.TonConnect(
